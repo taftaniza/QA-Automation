8156f381deb7728c86cfd6be4f4018bfe0ed5910
Taftaniza <taftanizalya@gmail.com>
2024-08-07 02:53:24 +0700

    empty practical branch

--- src/test/java/Enum.java
+++
@@ -1,9 +0,0 @@
-public class Enum {
-
-    enum Level {LOW, MEDIUM, HIGH}
-
-    public static void main(String[] args) {
-        Level myLevel = Level.MEDIUM;
-        System.out.println(myLevel);
-    }
-}
