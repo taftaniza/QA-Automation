f2b2cd2260da5f47bed3c1b13708a01398507086
Taftaniza <taftanizalya@gmail.com>
2024-08-06 13:47:41 +0700

    Basic java practise

---
+++ src/test/java/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle {
+
+    enum Level {LOW, MEDIUM, HIGH}
+
+    public static void main(String[] args) {
+        Level myLevel = Level.MEDIUM;
+        System.out.println(myLevel);
+    }
+}
