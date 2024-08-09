59ae6cb5b93fcaf1f9e4e315ebe56f29a4fea90b
Taftaniza <taftanizalya@gmail.com>
2024-08-09 12:08:36 +0700

    practise no.1

--- src/test/java/Practical1.java
+++ src/test/java/Practical1.java
@@ -18,6 +18,25 @@ public class Practical1
         }
     }
 
+    public void Square(int edge){
+        this.value=edge;
+        for (int i = 1; i <= edge; i++) {
+            for (int j = 1; j <= edge; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void Rectangle(int length, int width){
+        for(int i = 1; i<=width; i++){
+            for(int j = 1; j<=length; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main (String[] args) {
 
         System.out.println("Welcome to ASCII Shape Drawer\n");
@@ -41,11 +60,22 @@ public class Practical1
             a.Triangle(height);
         }
         else if(choice==2){
-            System.out.println("Enter the edges of the square: ");
+            System.out.println("Enter the edge of the square: ");
+            int edge = input.nextInt();
+            System.out.println("Enter the edge of the square: "+ edge);
+            Practical1 b = new Practical1();
+            b.Square(edge);
         }
         else if(choice==3){
             System.out.println("Enter the length of the rectangle: ");
+            int length = input.nextInt();
             System.out.println("Enter the width of the rectangle: ");
+            int width = input.nextInt();
+            System.out.println("length: "+ length);
+            System.out.println("width: "+ width);
+            Practical1 c = new Practical1();
+            c.Rectangle(length, width);
+
         }
         else if(choice==4){
             System.exit(0);
