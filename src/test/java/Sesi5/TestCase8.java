5da77da82093129d2c7fe3c91834acfd627bcb48
Taftaniza <taftanizalya@gmail.com>
2024-08-28 13:22:02 +0700

    test case 8 with allure

--- src/test/java/Sesi5/TestCase8.java
+++ src/test/java/Sesi5/TestCase8.java
@@ -4,24 +4,32 @@ package Sesi5;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WrapsDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class TestCase8 {
+    WebDriver driver;
 
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-        String baseUrl = "https://automationexercise.com/";
-        driver.get(baseUrl);
+        @BeforeClass
+                public void setUp()  {
+            driver = new ChromeDriver();
+            String baseUrl = "https://automationexercise.com/";
+            driver.get(baseUrl);
+        }
 
-        WebElement productsLink = driver.findElement(By.cssSelector("a[href='/products']"));
-        productsLink.click();
+        @Test(priority = 1)
+                public void test2() {
+            driver.findElement(By.cssSelector("a[href='/products']")).click();
+            driver.findElement(By.cssSelector("a[href='/product_details/1']")).click();
 
-        WebElement testCase = driver.findElement(By.cssSelector("a[href='/product_details/1']"));
-        testCase.click();
+            System.out.println("Navigated to detail product");
+        }
 
-        System.out.println("Navigated to detail product");
-    }
+        @AfterClass
+    public void tearDown() {
+            driver.quit();
+        }
+
 }
