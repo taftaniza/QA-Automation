fbdfb5637636f5a1b99f9902019ff8e714fb7c1e
Taftaniza <taftanizalya@gmail.com>
2024-08-28 13:19:57 +0700

    test case 7 with allure

--- src/test/java/Sesi5/TestCase7.java
+++ src/test/java/Sesi5/TestCase7.java
@@ -6,19 +6,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class TestCase7 {
 
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+    WebDriver driver;
+
+    @BeforeClass
+            public void beforeClass(){
+        driver = new ChromeDriver();
         String baseUrl = "https://automationexercise.com/";
         driver.get(baseUrl);
+    }
 
-        WebElement testCase = driver.findElement(By.linkText("Test Cases"));
-        testCase.click();
+    @Test(priority = 1)
+    public void testCase(){
+        driver.findElement(By.linkText("Test Cases")).click();
 
         System.out.println("Navigated to test cases page successfully");
+    }
 
-
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
     }
 }
